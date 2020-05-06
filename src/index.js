71d49b39f3b65c1321ad89251e321555adf91089
Sergio Perez <perez.sergio.ace@gmail.com>
2020-05-05 22:38:48 -0400

    cleaned up the index.js file

--- src/index.js
+++ src/index.js
@@ -1,25 +1,3 @@
-// import '../src/style.css'
-
-// let centerContent = document.getElementById('center-content')
-// let centerFold = document.getElementById('center-fold')
-
-// let foldsContent = Array.from(document.getElementsByClassName('fold-content'))
-
-// let targetScroll = -(document.documentElement.scrollTop || document.body.scrollTop)
-// let currentScroll = targetScroll
-// let tick = () => {
-// 	let overflowHeight = centerContent.clientHeight - centerFold.clientHeight
-
-// 	document.body.style.height = overflowHeight + window.innerHeight + 'px'
-
-// 	targetScroll = -(document.documentElement.scrollTop || document.body.scrollTop)
-// 	currentScroll += (targetScroll - currentScroll) * 0.1
-// 	foldsContent.forEach((content) => {
-// 		content.style.transform = `translateY(${currentScroll}px)`
-// 	})
-// 	requestAnimationFrame(tick)
-// }
-// tick()
 let centerContent = document.getElementById('center-content')
 let centerFold = document.getElementById('center-fold')
 
